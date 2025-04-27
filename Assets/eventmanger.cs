fb399da4ee96cc174097b43314945a5f3e87440a
Collin <17454325+Collin001998@users.noreply.github.com>
2025-04-27 19:19:56 +0200

    added new scene for profile select + at the start of the scene place canvas on camera hight

--- Assets/eventmanger.cs
+++ Assets/eventmanger.cs
@@ -5,7 +5,7 @@ public class eventmanger : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        this.transform.position = new Vector3(transform.position.x, Camera.main.transform.position.y, transform.position.z);
     }
 
     // Update is called once per frame
