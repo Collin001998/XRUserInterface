870b33d2b53ca6a54c375f87e2bbbe4f822a2d32
Collin <17454325+Collin001998@users.noreply.github.com>
2025-04-28 13:16:54 +0200

    added smoothing + moving threshold to magnifier (based on feedback)

--- Assets/MoveMagnifier.cs
+++ Assets/MoveMagnifier.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.Serialization;
 
 public class MoveMagnifier : MonoBehaviour
 {
     [SerializeField] private GameObject Magnifier;
     [SerializeField] private float rayDistance = 1f;
     [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float _smoothingFactor = 20f;
+    [SerializeField] private float _movingthreshold;
 
     private bool Active = true;
-
+    private Vector3 _lastGazePosition;
+    private Vector3 _newGazePosition;
+    private Vector3 _smoothingVector;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,8 +33,18 @@ public class MoveMagnifier : MonoBehaviour
             
             if (Physics.Raycast(transform.position, direction, out hit, Mathf.Infinity, layerMask)){
                 Magnifier.SetActive(true);
-                Magnifier.transform.position = hit.point;
-                Magnifier.transform.rotation = hit.transform.rotation;
+                _lastGazePosition = Magnifier.transform.position;
+                _newGazePosition = hit.point;
+
+                if ((_newGazePosition - _lastGazePosition).magnitude > _movingthreshold)
+                {
+                    //smooth position
+                    _smoothingVector = Vector3.Lerp(_smoothingVector, hit.point, Time.deltaTime * _smoothingFactor);
+                    //Magnifier.transform.position = hit.point;
+                    Magnifier.transform.position = _smoothingVector;
+                    Magnifier.transform.rotation = hit.transform.rotation;
+                }
+                
             }
             else if (Magnifier.activeSelf)
             {
