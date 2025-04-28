97f5f5afeb0752791bd6bb9a5818786bf3e3bffa
Collin <17454325+Collin001998@users.noreply.github.com>
2025-04-28 12:36:26 +0200

    added profile selection UI + made magnifier bigger

--- Assets/eventmanger.cs
+++ Assets/eventmanger.cs
@@ -1,11 +1,26 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class eventmanger : MonoBehaviour
 {
+    [SerializeField] OVRCameraRig _ovrCameraRig;
+    private bool _hasHeightAdjusted = false;
+
+    [Serializable] public enum ProfileType { Offset, Default, Narrowed};
+
+    public ProfileType profileType;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        this.transform.position = new Vector3(transform.position.x, Camera.main.transform.position.y, transform.position.z);
+        //add local method CamerarigUpdatedAnchors to the camera rig c# event
+        if (_ovrCameraRig != null)
+        {
+            _ovrCameraRig.UpdatedAnchors += CamerarigOnUpdatedAnchors;
+        }
+        
+
+        
     }
 
     // Update is called once per frame
@@ -14,8 +29,37 @@ public class eventmanger : MonoBehaviour
         
     }
 
-    public void Logpress()
+    private void OnDestroy()
     {
-        Debug.Log("pressed a button");
+        if (_ovrCameraRig != null)
+        {
+            _ovrCameraRig.UpdatedAnchors -= CamerarigOnUpdatedAnchors;
+        }
+    }
+
+    public void SetUIProfile(int profileIndex)
+    {
+        switch (profileIndex)
+        {
+            case 0: profileType = ProfileType.Offset; break;
+            case 1: profileType = ProfileType.Default; break;
+            case 2: profileType = ProfileType.Narrowed; break;
+        }
+
+        if (profileType == ProfileType.Default)
+        {
+            SceneManager.LoadScene("TemplateUI");
+        }
+        Debug.Log("pressed a button " + profileType);
+    }
+    void CamerarigOnUpdatedAnchors(OVRCameraRig obj)
+    {
+        if (_hasHeightAdjusted) return;
+        if (obj.centerEyeAnchor.transform.position.y < 0.2) return;
+        
+        transform.position = new Vector3(transform.position.x, obj.centerEyeAnchor.transform.position.y, transform.position.z);
+        _hasHeightAdjusted = true;
+        
+        
     }
 }
